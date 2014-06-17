9e64b851513bbfe95cb2afc6151d7bc03f2e8471
Smita <kharkatesmita@johndeere.com>
2014-06-17 18:33:55 +0530

    Commit without using stash

--- testorg.trial/src/main/java/com/vivek/App.java
+++ testorg.trial/src/main/java/com/vivek/App.java
@@ -6,11 +6,6 @@ package com.vivek;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
     public int add(int a, int b) {
         return a + b ;
     }
